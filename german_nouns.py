89bc1c46c17a2dc29ef5bd40245c4c7d980b833f
pajowu <git@ca.pajowu.de>
2021-12-13 21:07:19 +0100

    remove csv, load nouns from json

--- german_nouns.py
+++ german_nouns.py
@@ -1,17 +1,4 @@
-import csv
 import json
-import os.path
 
-if os.path.exists("nouns.json"):
-	with open("nouns.json") as f:
-		nouns = json.load(f)
-else:
-	nouns = []
-	with open('nouns.csv', newline='') as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			if row['pos'] == "Substantiv":
-				nouns.append({"lemma": row['lemma'], "genus": row['genus'], "pos": row['pos']})
-
-	with open("nouns.json", "w") as f:
-		json.dump(nouns, f)
+with open("nouns.json") as f:
+	nouns = json.load(f)
